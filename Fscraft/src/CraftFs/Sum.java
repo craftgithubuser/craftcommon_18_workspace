b23cc4bcc69f3f17c8a0f9fdbe1b4450bbeb7ed5
Feruza99 <126524657+Feruza99@users.noreply.github.com>
2023-03-03 10:25:55 -0500

    First Eclip

---
+++ Fscraft/src/CraftFs/Sum.java
@@ -0,0 +1,16 @@
+package CraftFs;
+
+public class Sum {
+
+	public static void main(String[] args) {
+       int a = 7;
+       int b= 10;
+       int sum;
+       
+       
+       sum= a+b;
+       System.out.println(sum);
+
+	}
+
+}
