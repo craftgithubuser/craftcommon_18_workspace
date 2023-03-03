6c6ae36629b3c59602b7ac89e2815f674bd25a06
Anteneh2023 <126181478+Anteneh2023@users.noreply.github.com>
2023-03-03 11:06:21 -0500

    added

--- Antenehjava/src/comm/cd/mysecondjava.java
+++ Antenehjava/src/comm/cd/mysecondjava.java
@@ -4,7 +4,7 @@ public class mysecondjava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("Hello Team");
+ System.out.println("Hello Team I added my first Java file ");
  System.out.println("Hello Team");
 	}
 
