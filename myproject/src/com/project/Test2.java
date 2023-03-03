af15bada37fcb330bc1f98cb52c4522a16a24d37
hiwiba <hiwiba23@gmail.com>
2023-03-03 00:26:15 -0500

    changed by hiwot

--- myproject/src/com/project/Test2.java
+++ myproject/src/com/project/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello team");
+		System.out.println("hello world");
+		System.out.println("hello classmate");
 	}
 
 }
