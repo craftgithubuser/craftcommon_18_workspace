ef37fe50e2fb578708da8e53b9cd018b258d2937
Teddybf <126130985+Teddybf@users.noreply.github.com>
2023-03-02 23:33:25 -0700

    commit from eclipse

---
+++ javaproject/src/craft18/test1.java
@@ -0,0 +1,13 @@
+package craft18;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("exercise");
+		System.out.println("exercise");
+		System.out.println("exercise");
+		System.out.println("exercise");
+	}
+
+}
