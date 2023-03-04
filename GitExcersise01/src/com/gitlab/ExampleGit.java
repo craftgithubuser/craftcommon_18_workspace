2bc585f3ee43edb2e6f4a76a78b90d3622ca8f5e
dm2638 <126540522+dm2638@users.noreply.github.com>
2023-03-03 20:44:56 -0500

    Example commited

---
+++ GitExcersise01/src/com/gitlab/ExampleGit.java
@@ -0,0 +1,12 @@
+package com.gitlab;
+
+public class ExampleGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
