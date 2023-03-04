81af405254664aeb95ca0f63d42948c40c1d2d18
dm2638 <126540522+dm2638@users.noreply.github.com>
2023-03-03 19:35:50 -0500

    commit this

---
+++ DanielSoriProject/src/com/daniel/exaple02.java
@@ -0,0 +1,13 @@
+package com.daniel;
+
+public class exaple02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello class");
+		System.out.println("hello class");
+		
+	}
+
+}
