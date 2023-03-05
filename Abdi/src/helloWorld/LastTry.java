333d2fb2da7f2c3ebc6bad7de9002e36002a92ba
Abdit1 <abditaye3@gmail.com>
2023-03-04 23:05:29 -0500

    deleted output, only for loop

--- Abdi/src/helloWorld/LastTry.java
+++ Abdi/src/helloWorld/LastTry.java
@@ -4,8 +4,7 @@ public class LastTry {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("whats going on?");
-		System.out.println("still figuring it out");
+		System.out.println("This is numbers");
 		int[] numbers = {1, 2, 3, 4, 5};
 		for (int num : numbers) {
 		    System.out.println(num);
