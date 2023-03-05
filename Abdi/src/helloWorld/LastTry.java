b25ce8dedf52dae3664fc52dda0294382ceaaa01
Abdit1 <abditaye3@gmail.com>
2023-03-04 23:02:13 -0500

    added for loop to my code

--- Abdi/src/helloWorld/LastTry.java
+++ Abdi/src/helloWorld/LastTry.java
@@ -6,6 +6,11 @@ public class LastTry {
 		// TODO Auto-generated method stub
 		System.out.println("whats going on?");
 		System.out.println("still figuring it out");
+		int[] numbers = {1, 2, 3, 4, 5};
+		for (int num : numbers) {
+		    System.out.println(num);
+		}
+
 	}
 
 }
