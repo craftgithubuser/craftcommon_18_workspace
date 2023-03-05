e40057d2cc5505912c7390f0cc9bc866a70ddba0
Abdit1 <abditaye3@gmail.com>
2023-03-04 21:53:34 -0500

    changes

--- Abdi/src/helloWorld/LastTry.java
+++ Abdi/src/helloWorld/LastTry.java
@@ -5,6 +5,7 @@ public class LastTry {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("whats going on?");
+		System.out.println("still figuring it out");
 	}
 
 }
