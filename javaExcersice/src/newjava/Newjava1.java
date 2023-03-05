6c81c436ab12ca0bd19402c597d32c51b1de4383
Tsion2001 <batinawa@yahoo.com>
2023-03-05 15:43:40 -0800

    commit2

--- javaExcersice/src/newjava/Newjava1.java
+++ javaExcersice/src/newjava/Newjava1.java
@@ -5,7 +5,10 @@ public class Newjava1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	
 		System.out.println("print something");
+		System.out.println("try");
 	}
+	
 
 }
