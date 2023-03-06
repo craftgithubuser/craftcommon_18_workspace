512c4a01d5105fa55e564f0061b10fa89a3a5e56
Aman-birhanu <amanutd21@gmail.com>
2023-03-05 22:04:14 -0500

    commit

--- aman1982/src/aman1982/Good1982.java
+++ aman1982/src/aman1982/Good1982.java
@@ -4,7 +4,7 @@ public class Good1982 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("union");
+System.out.println("union eclipse");
 
 		
 		
