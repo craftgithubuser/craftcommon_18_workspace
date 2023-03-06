a06f40b64d791e8e92c614c36e8f1ca6597a7568
Yared <Yaredgg16@gmail.com>
2023-03-05 18:03:55 -0800

    this is Yareds Thursday commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+       /* System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        //changed1jghkygkhhjhughgghbfj
+        //changed1jghkygkhhjhughgghbfj*/
+
+        MyClass myObject = new MyClass();
+        int sum1 = myObject.add(2, 3);           // Calls the first add() method
+        double sum2 = myObject.add(2.5, 3.5);    // Calls the second add() method
+        String str = myObject.add("Hello", "World");   // Calls the third add() method
+
     }
 }
---
+++ src/MyClass.java
@@ -0,0 +1,16 @@
+public class MyClass {
+
+        public int add(int x, int y) {
+            return x + y;
+        }
+
+        public double add(double x, double y) {
+            return x + y;
+        }
+
+        public String add(String x, String y) {
+            return x + y;
+        }
+
+
+}
