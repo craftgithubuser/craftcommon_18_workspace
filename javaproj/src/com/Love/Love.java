00eeb0489dbed5fe6004f303a3075853db2289dc
nhtty <126219176+nhtty@users.noreply.github.com>
2023-03-06 01:12:19 -0500

    love letter from eclipse

---
+++ javaproj/src/com/Love/Love.java
@@ -0,0 +1,30 @@
+package com.Love;
+
+public class Love {
+
+	public static void main(String[] args) {
+		String you = "you";
+		String me = "me";
+		String us = you + " and " + me;
+
+		System.out.println("When I first met " + you + ", my heart skipped a beat.");
+		System.out.println("I knew right then and there that " + you + " were something sweet.");
+		System.out.println("I couldn't help but feel drawn to " + you + " like a magnet,");
+		System.out.println("And every time we talked, my heart felt like it had a reset.");
+
+		System.out.println("\nNow, " + you + " and " + me + " are inseparable,");
+		System.out.println("Like two strings of code that can't be divisible.");
+		System.out.println("We complement each other like classes and objects,");
+		System.out.println("And when we're together, time seems to lose its concepts.");
+
+		System.out.println("\n" + you + " make my world a better place,");
+		System.out.println("Like a method that always returns a positive case.");
+		System.out.println("I promise to love " + you + " like a constant,");
+		System.out.println("And be " + you + "'s support like a try-catch statement.");
+
+		System.out.println("\n" + us + " are like a program that's perfectly compiled,");
+		System.out.println("A love so true, it can never be uninstalled.");
+		System.out.println("I'll be " + you + "'s partner for life like a Java class,");
+		System.out.println("Our love story, an endless loop that will forever last.");
+	}
+}
