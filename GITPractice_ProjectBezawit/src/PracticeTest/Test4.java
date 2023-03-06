e7d1490315f87ee03613fc721fea1283cf1b26cb
Bezawit Tefera <bezawittefera123@gmail.com>
2023-03-05 22:26:39 -0500

    Test4 first commit

---
+++ GITPractice_ProjectBezawit/src/PracticeTest/Test4.java
@@ -0,0 +1,11 @@
+package PracticeTest;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Changes have been made.");
+
+	}
+
+}
