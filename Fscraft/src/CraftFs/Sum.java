4cfcb3595e95dc2b48ab0d0134030ae46feda66b
Feruza99 <126524657+Feruza99@users.noreply.github.com>
2023-03-06 20:04:39 -0500

    Added opera

--- Fscraft/src/CraftFs/Sum.java
+++ Fscraft/src/CraftFs/Sum.java
@@ -6,12 +6,24 @@ public class Sum {
        int a = 7;
        int b= 10;
        int sum;
+       double z=  Div(100.4, 20.2);
+       System.out.println(z);
+       double y=  Div(2500.4, 2.2);
+       System.out.println(y);
        
        
        sum= a+b;
        System.out.println(sum);
        System.out.println("Java");
 
+	}
+	public static double Div(double a, double b) {
+		
+		double z;
+		z=  a/b;
+		
+		return z;
+		
 	}
 
 }
