e5ba86f393b4e2697dd192e5890102d30b79043c
Behailucraft <behailutesfaye51@gmail.com>
2023-03-08 16:24:56 -0500

    newone

--- Behailujavasample/src/com/project/newclass.java
+++ Behailujavasample/src/com/project/newclass.java
@@ -4,6 +4,7 @@ public class newclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 System.out.println("Respect to you all");
 System.out.println("salute");
 
