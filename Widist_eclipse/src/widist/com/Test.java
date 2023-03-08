882c8da3056257a7b6b233c64f155b900893dabd
widistdawit <widistbantirga@gmail.com>
2023-03-08 16:54:15 -0500

    commit multiple changes.

--- Widist_eclipse/src/widist/com/Test.java
+++ Widist_eclipse/src/widist/com/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome");
+		System.out.println("Welcome to craft knowledge");
+		System.out.println("Welcome to the team");
 	}
 
 }
