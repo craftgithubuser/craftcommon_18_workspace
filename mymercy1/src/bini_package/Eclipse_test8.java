d490abab9e8cc45c93565e520bb2a9709babf534
Kebron2020 <126136336+Kebron2020@users.noreply.github.com>
2023-03-07 20:08:41 -0700

    bini eclpse push 8

---
+++ mymercy1/src/bini_package/Eclipse_test8.java
@@ -0,0 +1,10 @@
+package bini_package;
+
+public class Eclipse_test8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("bini eclipse push 8");
+	}
+
+}
