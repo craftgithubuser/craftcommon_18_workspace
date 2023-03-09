3923bad33f126a8ca899e112bb27cdddebcbc3cb
sisumerga18 <sisumerga18@gmail.com>
2023-03-08 19:57:12 -0500

    eclips commit

--- craftcommon_18_workspace/sample project 2/src/Elshu16/exercise1.java
+++ craftcommon_18_workspace/sample project 2/src/Elshu16/exercise1.java
@@ -8,7 +8,7 @@ public class exercise1 {
 		System.out.println ("hello guys");
 		System.out.println ("hello guys");
 		System.out.println ("hello guys");
-		
+        System.out.println ("hello guys");
 
 	}
 
---
+++ craftcommon_18_workspace/sample project 2/src/ethio/addis.java
@@ -0,0 +1,11 @@
+package ethio;
+
+public class addis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub ("capital city of Ethiopia")
+			
+		System.out.println;
+	}
+
+}
