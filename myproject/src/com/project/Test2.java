c682c7417a48e08b3ccdb0ab00f813312ca785f6
hiwiba <hiwiba23@gmail.com>
2023-03-12 18:02:00 -0400

    committed from eclipse

--- myproject/src/com/project/Test2.java
+++ myproject/src/com/project/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("heloo world");
+		System.out.println("starbuks");
 	}
 
 }
